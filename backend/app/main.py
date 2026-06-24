abde1343e7fff43b913f7b62af40424ff9e91cef
Yahzir Hood <yahyaboss1218@gmail.com>
2026-06-24 05:27:02 -0400

    Added the route for Comparison

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI 
 from .lifespan import lifespan
 from backend.app.routers.lastfm import router as lastfm_router
+from backend.app.routers.comparison import router as comparison_router
 
 app = FastAPI(lifespan=lifespan)
 
-# Register the routes from the router file
+# Register the routes from the router directory
 app.include_router(lastfm_router)
-
+app.include_router(comparison_router)
