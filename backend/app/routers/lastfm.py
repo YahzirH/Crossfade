45b72757196f0b760b5dac0b4c654478446b0ec8
Yahzir Hood <yahyaboss1218@gmail.com>
2026-06-23 23:03:12 -0400

    Implemented UserStats filter to only display data specified in schema

--- backend/app/routers/lastfm.py
+++ backend/app/routers/lastfm.py
@@ -1,8 +1,19 @@
 from fastapi import APIRouter, Depends
 from backend.app.services.lastfm import LastFMService
+from backend.app.schemas.stats import UserStats
 
 router = APIRouter(prefix="/lastfm", tags=["lastfm"])
 
-@router.get("/user/{username}")
+@router.get("/user/{username}", response_model=UserStats)
 async def get_user_info(username: str, service: LastFMService = Depends()):
-    return await service.get_user_info(username)
+    data = await service.get_user_info(username)
+
+    user = data["user"]
+
+    return UserStats(
+        username=user["name"],
+        playcount=int(user["playcount"]),
+        artist_count=int(user["artist_count"]),
+        track_count=int(user["track_count"]),
+        album_count=int(user["album_count"]),
+    )
