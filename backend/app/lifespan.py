35c72a1d703b2c3d614b8b37842c153b2b63ca96
Yahzir Hood <yahyaboss1218@gmail.com>
2026-06-26 23:44:51 -0400

    Added logging for opening and closing http client connection

--- backend/app/lifespan.py
+++ backend/app/lifespan.py
@@ -1,6 +1,10 @@
-from contextlib import asynccontextmanager
+import logging
 import httpx 
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+
+# Initialize a logger for this module
+logger = logging.getLogger(f"crossfade.{__name__}")
 
 _http_client: httpx.AsyncClient | None = None
 
@@ -8,7 +12,9 @@ _http_client: httpx.AsyncClient | None = None
 async def lifespan(app: FastAPI):
     global _http_client
     _http_client = httpx.AsyncClient(base_url="http://ws.audioscrobbler.com/2.0/")
+    logger.info("HTTP client initialized.")
     yield
+    logger.info("Closing HTTP client.")
     await _http_client.aclose()
 
 def get_global_http_client() -> httpx.AsyncClient:
