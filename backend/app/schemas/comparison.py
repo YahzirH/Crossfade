f6374b46dc542933bbc44cead82458a382592229
Yahzir Hood <yahyaboss1218@gmail.com>
2026-06-27 00:24:22 -0400

    Added scores to the UserComparison model

--- backend/app/schemas/comparison.py
+++ backend/app/schemas/comparison.py
@@ -5,4 +5,6 @@ class UserComparison(BaseModel):
     user_b: str
     shared_artists: list[str]
     shared_tracks: list[str]
+    artist_score: float
+    track_score: float
     overall_score: float
